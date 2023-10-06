854f1b4ee17bc81c18893a39402a099890fa8944
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2023-10-06 18:11:00 +0200

    Add Program Flowchart

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
 #include "motor/movement.h"
+//#include "controller/controller.h"
 
 Movement movement;
+//XboxController controller;
 
 void setup() {
   Serial.begin(115200);
   delay(500);
   Serial.println(("Firmware version: " + std::string(constants::version)).c_str());
   movement.init();
-  movement.SPEED = 500;
+  //controller.init();
+  movement.SPEED = MAX_MOTOR_SPEED;
   movement.DIRECTION = 0;
   movement.Apply();
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
+  for(int i = 0; i < 360; i++)
+  {
+    movement.DIRECTION = i;
+    movement.Apply();
+    delay(5);
+  }
 }
