d66f84a23537d5cde583484dfd0ac42ea6e86755
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2023-11-07 11:30:15 +0100

    Measure Ultrasonic Sonic Sensor Distance

---
+++ Code/src/ultrasonic-sensors/uss.h
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+class uss
+{
+    private:
+
+
+    public:
+    uint8_t echoPin, triggerPin;
+    uss(uint8_t _echoPin, uint8_t _triggerPin)
+    {
+        this->echoPin = _echoPin;
+        this->triggerPin = _triggerPin;
+    }
+
+    //@brief returns the distance in cm
+    int MeasureDistance()
+    {
+        // Clears the trigPin
+        digitalWrite(triggerPin, LOW);
+        delayMicroseconds(2);
+        // Sets the trigPin on HIGH state for 10 micro seconds
+        digitalWrite(triggerPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(triggerPin, LOW);
+        // Reads the echoPin, returns the sound wave travel time in microseconds
+        long duration = pulseIn(echoPin, HIGH);
+        // Calculating the distance
+        int distance = duration * 0.034 / 2;
+        // Prints the distance on the Serial Monitor
+        return distance;
+    }
+};
