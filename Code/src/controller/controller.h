a6f550d0b7ea06f7ab81f7aae514ebcf85605aa6
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2024-02-06 11:01:16 +0100

    Add Detection for Konami Code

--- Code/src/controller/controller.h
+++ Code/src/controller/controller.h
@@ -2,22 +2,54 @@
 #include "../vars/constants.h"
 
 class Controller
-{
+{   
+    private:
+    int8_t buttonCirclePrev, buttonXPrev, dPadUpPrev, dPadRightPrev, dPadDownPrev, dPadLeftPrev, buttonStartPrev;
+    enum KonamiCodeButtons
+    {
+        KONAMI_BUTTON_NONE,
+        KONAMI_BUTTON_UP,
+        KONAMI_BUTTON_DOWN,
+        KONAMI_BUTTON_LEFT,
+        KONAMI_BUTTON_RIGHT,
+        KONAMI_BUTTON_B,
+        KONAMI_BUTTON_A,
+        KONAMI_BUTTON_START
+    };
+    KonamiCodeButtons konamiButtonList[11];
     public:
-    int8_t joyLX, joyLY, joyRX, joyRY, buttonSelect, buttonSquare, buttonCircle, buttonTriangle, dPadUp, dPadRight, dPadDown, dPadLeft, joyRButton, joyLButton;
+    int8_t joyLX, joyLY, joyRX, joyRY, buttonSelect, buttonStart, buttonSquare, buttonCircle, buttonTriangle, buttonX, dPadUp, dPadRight, dPadDown, dPadLeft, joyRButton, joyLButton, shoulderButtonL, shoulderButtonR;
     ps3_status_battery battery;
     uint8_t throttleGas, throttleBreake;
     unsigned long led_blink_millis;
-    bool led_blink;
+    bool led_blink, konamiCode, konamiCodePrev;
+
+
 
     void init()
     {
         Ps3.attachOnConnect(OnConnect);
         Ps3.begin(constants::controller_vars::BT_ADDR.c_str());
+        for(uint8_t i = 0; i < 11; i++)
+        {
+            konamiButtonList[i] = KONAMI_BUTTON_NONE;
+        }
+        konamiCode = false;
+        konamiCodePrev = false;
     }
 
     void loop()
     {
+        buttonCirclePrev = buttonCircle;
+        buttonXPrev = buttonX;
+        dPadUpPrev = dPadUp;
+        dPadRightPrev = dPadRight;
+        dPadDownPrev = dPadDown;
+        dPadLeftPrev = dPadLeft;
+        buttonStartPrev = buttonStart;
+        konamiCodePrev = konamiCode;
+
+
         joyLX = Ps3.data.analog.stick.lx;
         joyLY = Ps3.data.analog.stick.ly;
         joyRX = Ps3.data.analog.stick.rx;
@@ -41,15 +73,14 @@ class Controller
         buttonCircle   = Ps3.data.button.circle;
         buttonTriangle = Ps3.data.button.triangle;
 
+        shoulderButtonL = Ps3.data.button.l1;
+        shoulderButtonR = Ps3.data.button.r1;
+
 
         if(millis() - led_blink_millis > 250)
         {
             led_blink_millis = millis();
             led_blink = !led_blink;
-        }
-        else
-        {
-            return;
         }
 
         switch(Ps3.data.status.battery)
@@ -77,6 +108,8 @@ class Controller
                 }
                 break;
         }
+
+        CheckKonami();
     }
 
     static void OnConnect()
@@ -86,5 +119,42 @@ class Controller
         Ps3.setRumble(100.0, 250);
         delay(500);
         Ps3.setRumble(100.0, 250);
+    }
+
+    void CheckKonami()
+    {
+        for(uint8_t i = 0; i < 10; i++)
+        {
+            konamiButtonList[i] = konamiButtonList[i+1];
+        }
+
+        if(dPadUp && !dPadUpPrev)
+            konamiButtonList[10] = KONAMI_BUTTON_UP;
+        else if(dPadDown && !dPadDownPrev)
+            konamiButtonList[10] = KONAMI_BUTTON_DOWN;
+        else if(dPadLeft && !dPadLeftPrev)
+            konamiButtonList[10] = KONAMI_BUTTON_LEFT;
+        else if(dPadRight && !dPadRightPrev)
+            konamiButtonList[10] = KONAMI_BUTTON_RIGHT;
+        else if(buttonCircle && !buttonCirclePrev)
+            konamiButtonList[10] = KONAMI_BUTTON_B;
+        else if(buttonX && !buttonXPrev)
+            konamiButtonList[10] = KONAMI_BUTTON_A;
+        else if(buttonStart && !buttonStart)
+            konamiButtonList[10] = KONAMI_BUTTON_START;
+
+        if(konamiButtonList[10] == KONAMI_BUTTON_START &&
+           konamiButtonList[9] == KONAMI_BUTTON_A &&
+           konamiButtonList[8] == KONAMI_BUTTON_B &&
+           konamiButtonList[7] == KONAMI_BUTTON_RIGHT &&
+           konamiButtonList[6] == KONAMI_BUTTON_LEFT &&
+           konamiButtonList[5] == KONAMI_BUTTON_RIGHT &&
+           konamiButtonList[4] == KONAMI_BUTTON_LEFT &&
+           konamiButtonList[3] == KONAMI_BUTTON_DOWN &&
+           konamiButtonList[2] == KONAMI_BUTTON_DOWN &&
+           konamiButtonList[1] == KONAMI_BUTTON_UP &&
+           konamiButtonList[0] == KONAMI_BUTTON_UP &&
+           konamiCodePrev == false)
+           konamiCode = true;
     }
 };
